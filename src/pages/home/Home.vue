8bc4cc1dc7f5aeacd7df64dba45bbca5cc5d2971
KEWEN <kewen@huazhu.com>
2018-06-03 20:56:27 +0800

    'init'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,31 @@
+<!--  -->
+<template>
+  <div>home</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: [],
+
+  data () {
+    return {
+    };
+  },
+
+  created(){},
+
+  mounted(){},
+
+  watch: {},
+
+  computed: {},
+
+  methods: {},
+
+  components: {}
+}
+
+</script>
+<style scoped>
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,31 @@
+<!--  -->
+<template>
+  <div>list</div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: [],
+
+  data () {
+    return {
+    };
+  },
+
+  created(){},
+
+  mounted(){},
+
+  watch: {},
+
+  computed: {},
+
+  methods: {},
+
+  components: {}
+}
+
+</script>
+<style scoped>
+</style>
