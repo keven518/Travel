5b4973d1e89ed38c857efd6f68e735d3cd84798f
KEWEN <kewen@huazhu.com>
2018-06-13 14:57:27 +0800

    'city'

---
+++ src/pages/city/City.vue
@@ -0,0 +1,35 @@
+<!--  -->
+<template>
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import cityHeader from './components/Header'
+export default {
+  name: 'City',
+
+  data () {
+    return {
+    };
+  },
+
+  created(){},
+
+  mounted(){},
+
+  watch: {},
+
+  computed: {},
+
+  methods: {},
+
+  components: {
+    cityHeader
+  }
+}
+
+</script>
+<style lang="stylus" scoped>
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,28 +8,30 @@
       <span class='iconfont'>&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{city}}
-      <span class='iconfont arrow-icon'>&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{city}}
+        <span class='iconfont arrow-icon'>&#xe64a;</span>
+      </div>
+    </router-link>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeHeader',
+  name: "HomeHeader",
   props: {
     city: String
   },
 
-  data () {
-    return {
-    };
+  data() {
+    return {};
   },
 
-  created(){},
+  created() {},
 
-  mounted(){},
+  mounted() {},
 
   watch: {},
 
@@ -38,37 +40,48 @@ export default {
   methods: {},
 
   components: {}
-}
-
+};
 </script>
 <style lang="stylus" scoped>
-  @import '~styles/varibles.styl'
-  .header
-    display: flex
-    line-height: .86rem
-    background: $bgColor
+@import '~styles/varibles.styl';
+
+.header {
+  display: flex;
+  line-height: $headerHeight;
+  background: $bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+
+    .back-icon {
+      text-align: center;
+      font-size: 0.32rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    background: #fff;
+    border-radius: 0.1rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
     color: #fff
-    .header-left
-      width: .64rem
-      float: left
-      .back-icon
-        text-align: center
-        font-size: .4rem
-    .header-input
-      flex: 1
-      height .64rem
-      line-height: .64rem
-      margin-top: .12rem
-      margin-left: .2rem
-      padding-left: .2rem
-      background: #fff
-      border-radius: .1rem
-      color: #ccc;
-    .header-right
-      width: 1.24rem
-      float: right
-      text-align: center
-      .arrow-icon
-        margin-left: -.04rem
-        font-size: .24rem
+    .arrow-icon {
+      margin-left: -0.04rem;
+      font-size: 0.24rem;
+    }
+  }
+}
 </style>
