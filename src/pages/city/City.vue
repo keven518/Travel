86f0812bf0886c3c2ab3571cd61f4aae45d1b2e2
KEWEN <kewen@huazhu.com>
2018-06-20 20:34:10 +0800

    'city-ajax'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,12 +3,13 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import cityHeader from './components/Header'
 import citySearch from './components/Search'
 import cityList from './components/List'
@@ -18,18 +19,36 @@ export default {
 
   data () {
     return {
+      cities: {},
+      hotCities: []
     };
   },
 
   created(){},
 
-  mounted(){},
+  mounted(){
+    this.getCityInfo()
+  },
 
   watch: {},
 
   computed: {},
 
-  methods: {},
+  methods: {
+    getCityInfo() {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      console.log(res)
+      res = res.data
+      if(res.ret && res.data){
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
 
   components: {
     cityHeader,
