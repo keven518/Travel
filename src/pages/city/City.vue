6e3dfabeb5d52a9ecde2f32edc7039f112fd0b5a
KEWEN <kewen@huazhu.com>
2018-06-20 20:06:03 +0800

    'city-list'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,12 +3,16 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import cityHeader from './components/Header'
 import citySearch from './components/Search'
+import cityList from './components/List'
+import cityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
 
@@ -29,7 +33,9 @@ export default {
 
   components: {
     cityHeader,
-    citySearch
+    citySearch,
+    cityList,
+    cityAlphabet
   }
 }
 
